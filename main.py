951d5df3bd8229a5586358cd6ee7bcd19f3820ec
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-04-27 17:18:37 +0100

    a code that prints numbers

---
+++ main.py
@@ -0,0 +1,7 @@
+num_of_js = 5
+def pattern1(num_of_js):
+    for i in range(num_of_js, 0, -1):
+        for j in range(i):
+             print(i, end = " ")
+        print("\n")
+pattern1(num_of_js)
